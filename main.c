1b614b1b4c88761d411f583451de09050a1f4261
josepappa <arg23149@uvg.edu.gt>
2025-04-02 14:34:19 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+/*
+ * PreLab4.c
+ *
+ * Created: 4/1/2025 7:05:54 PM
+ * Author : jargu
+ */ 
+
+#define	F_CPU 16000000
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
